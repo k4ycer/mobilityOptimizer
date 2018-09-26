72da04a9ffc7c2d063c1b7fa777415e73337343b
epicLevi <tomas.carbellido@gmail.com>
2018-09-26 14:13:35 -0500

    Added function randbreak()

--- Program.cs
+++ Program.cs
@@ -9,12 +9,35 @@ namespace mobilityOptimizer
             
         }
 
-        static void CalcTourLength() {
+        static void Mdmtspv_ga(double[,] xy, int max_salesmen, double depots, int CostType, int min_tour, int pop_size, int num_iter, bool show_prog, bool show_res, double[,] dmat){
             
         }
 
-        static void Mdmtspv_ga(float[,] xy, int max_salesmen, float depots, int CostType, int min_tour, int pop_size, int num_iter, bool show_prog, bool show_res, float[,] dmat){
-            
+        static double[] randbreak(int max_salesmen, int n, int min_tour) {
+            double[] breaks;
+
+            int num_brks = max_salesmen - 1;
+            breaks = new double[num_brks];
+
+            for (int i = 0; i < num_brks; i++)
+            {
+                double newRand = new Random().Next(1, n);
+
+                foreach (double item in breaks)
+                {
+                    if (item == newRand) 
+                    {
+                        i--;
+                        continue;
+                    }
+                }
+
+                breaks[i] = newRand;
+            }
+
+            Array.Sort(breaks);
+
+            return breaks;
         }
     }
 }
